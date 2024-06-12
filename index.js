60df1409aa6f8d56666d5308a59306f6afbd65ea
Bailey Benoit <Bailey.L.Benoit@gmail.com>
2024-06-12 14:49:06 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+// function findMatching(drivers, query) {
+//     return drivers.filter(driver => {
+
+//       const lowercasedDriver = driver.toLowerCase();
+//       const lowercasedQuery = query.toLowerCase();
+
+//       return lowercasedDriver === lowercasedQuery;
+//     });
+//   }
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
